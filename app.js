c93fbcb055cfeb6f68ba2909f7a267fede93e3bf
Fachri Hawari <fachri.hawari@gmail.com>
2025-06-02 16:05:00 +0700

    feat: Register done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
+
 const homeRouter = require('./routers/home_router')
+const authRouter = require('./routers/auth_router')
 
 const app = express()
 const port = 3000
@@ -7,6 +9,7 @@ const port = 3000
 app.use(express.json())
 
 app.use(homeRouter)
+app.use(authRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
